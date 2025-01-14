55bed2586b352a0e45e93c8b274dd1214ddaa45a
lomonchiapp <lomonchiapp@gmail.com>
2025-01-14 07:56:22 -0400

    verifica si se agregan imagenes

--- src/components/VehicleDialog/index.tsx
+++ src/components/VehicleDialog/index.tsx
@@ -18,7 +18,7 @@ export function VehicleDialog({ openDialog, setOpenDialog, children }: VehicleDi
   return (
     <Dialog open={openDialog} onOpenChange={setOpenDialog}>
       <DialogContent className={cn(
-        "bg-white rounded-lg p-5 max-h-[90vh] min-w-[70vw] pt-12",
+        "bg-white rounded-lg p-5 max-h-[90vh] min-w-[70vw] pt-10",
         "w-[95vw] overflow-y-auto sm:w-[90vw]  md:w-[85vw] lg:w-[75vw] xl:w-[1200px]",
       )}>
         <button
